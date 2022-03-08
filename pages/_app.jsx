4bb8a09680ecfdf1d88519ac3b1c259a5b02027b
Christopher Lang <chrsalxlng@gmail.com>
2022-03-08 15:34:52 +0100

    fix: update favicon and page title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>jdlang.de</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
